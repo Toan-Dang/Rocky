507dc565f365240a6ab0b46e3e1e2fce7be3c7bb
Toan Dang <19522357@gm.uit.edu.vn>
2021-01-19 12:34:38 +0700

    ?

---
+++ Rocky - Copy/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers {
+
+    public class CategoryController : Controller {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        public IActionResult Index() {
+            IEnumerable<Category> objlist = _db.Category;
+            return View(objlist);
+        }
+
+        //GET-Create
+        public IActionResult Create() {
+            return View();
+        }
+
+        //Post- Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj) {
+            if(ModelState.IsValid) {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //get-edit
+        public IActionResult Edit(int? id) {
+            if(id == null || id == 0) {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if(obj == null) {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //Post- Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj) {
+            if(ModelState.IsValid) {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //get-delete
+        public IActionResult Delete(int? id) {
+            if(id == null || id == 0) {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if(obj == null) {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //Post- delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id) {
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+                return NotFound();
+
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
